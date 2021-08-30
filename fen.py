080ffe9465485abe93e30b271b07fb3f6907aadf
Ivan Asonov <asonovivan88@gmail.com>
2021-08-30 20:33:10 +0200

    - add methods to encrypt/decrypt file content

--- fen.py
+++ fen.py
@@ -22,13 +22,44 @@ def load_key():
     return open(home + "/fencrypt.key").read()
 
 
+def encrypt(filename, key=load_key()):
+    """
+    Given a filename (str) with path included and key (bytes), it encrypts the content of the file and write it
+    """
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        file_content = file.read()
+
+    encrypted = f.encrypt(file_content)
+
+    with open(filename, "wb") as file:
+        file.write(encrypted)
+
+
+def decrypt(filename, key=load_key()):
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        decrypted = file.read()
+
+    content = f.decrypt(decrypted)
+
+    with open(filename, "wb") as file:
+        file.write(content)
+
+
 if __name__ == "__main__":
     # write_key()
-    key = load_key()
-    f = Fernet(key)
-    message = sys.argv[1].encode()
-    print(message)
-    encrypted = f.encrypt(message)
-    print(encrypted)
-    decrypted = f.decrypt(encrypted)
-    print(decrypted)
+    action = sys.argv[1]
+    full_path_to_file = sys.argv[2]
+    if "-d" == action:
+        decrypt(full_path_to_file)
+    elif "-e" == action:
+        encrypt(full_path_to_file)
+    else:
+        print("unknown key" + action)
+
+
+class Encryptor:
+    """
+    This class should encapsulate the logic relate to the encryption
+    """
